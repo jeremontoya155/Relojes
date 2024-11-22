054d2d89098a977fb5850589bb9ac35f32bc6f54
Jeremias <jeremontoya155@gmail.com>
2024-11-22 16:06:05 -0300

    Documentracion nueva con cambios ticket por hora

--- server.js
+++ server.js
@@ -9,24 +9,28 @@ const app = express();
 // Configurar CORS para permitir solicitudes de todos los orígenes
 app.use(cors());
 
+
 // Puerto desde el archivo .env o por defecto 3000
 const port = process.env.PORT || 3000;
 
 // Configurar EJS como motor de plantillas
 app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'views'));
+app.use(express.static(path.join(__dirname, 'public')));
 
 // Middleware para analizar solicitudes POST (form-data) y JSON
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
 // Importar rutas
-const attendanceRoutes = require('./routes/attendanceRoutes'); // Rutas para la funcionalidad principal
+const attendanceRoutes = require('./routes/attendanceRoutes'); // Rutas para la funcionalidad de asistencia
 const documentationRoutes = require('./routes/documentationRoutes'); // Rutas para la documentación
+const ticketRoutes = require('./routes/ticketRoutes'); // Rutas para tickets por hora
 
 // Usar rutas
 app.use('/', attendanceRoutes); // Rutas para la funcionalidad de asistencia
 app.use('/documentation', documentationRoutes); // Rutas para la documentación
+app.use('/tickets', ticketRoutes); // Rutas para tickets por hora
 
 // Manejar errores 404 (Página no encontrada)
 app.use((req, res) => {
